37e1c0f7acb6a96545b3e0d335b0536dbdbc1212
hmo.jreyes@gmail.com <hmo.jreyes@gmail.com>
2026-09-04 05:21:50 +0000

    Edited Menu

--- client_code/Menu/__init__.py
+++ client_code/Menu/__init__.py
@@ -10,6 +10,21 @@ class Menu(MenuTemplate):
     except Exception:
       pass
 
+    try:
+      url_hash = anvil.get_url_hash()
+      if isinstance(url_hash, str) and url_hash:
+        url_lower = url_hash.lower()
+        if 'pos_mesero' in url_lower or 'croquis' in url_lower:
+          anvil.open_form('POSMesero')
+        elif 'monitor_cocina' in url_lower or 'kds' in url_lower:
+          anvil.open_form('MonitorCocina')
+        elif 'monitor_fiscal' in url_lower or 'fiscal' in url_lower:
+          anvil.open_form('MonitorFiscal')
+        elif 'clientes_lealtad' in url_lower or 'rewards' in url_lower:
+          anvil.open_form('ClientesLealtad')
+    except Exception as e:
+      print(f"Error procesando routing en Menu: {e}")
+
   def navegar_modulo(self, modulo_nombre):
     target_form = 'POSMesero'
     if modulo_nombre in ['pos_mesero', 'croquis', 'palapa']:
@@ -22,5 +37,5 @@ class Menu(MenuTemplate):
       target_form = 'MonitorFiscal'
     elif modulo_nombre in ['clientes_lealtad', 'rewards']:
       target_form = 'ClientesLealtad'
-    
+
     anvil.open_form(target_form)
